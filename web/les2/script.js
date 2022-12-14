afbad89dc5e6a8527a72a36513dc0443e8b1f03f
jamydewaal <35739@ma-web.nl>
2022-12-14 14:25:33 +0100

    hulp

--- web/les2/script.js
+++ web/les2/script.js
@@ -1,1 +1,18 @@
 console.log("script is geladen");
+
+const groentevak = document.getElementById('Groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = 'Hoi';
+    fruitvak.appendChild(nieuwElement);
+}
+function zetInGroenteLa(groente){
+    console.log(groente)
+
+    
+}
+
