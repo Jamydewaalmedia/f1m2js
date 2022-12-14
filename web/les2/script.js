d5d07a6828712d072c38f44cd4b69469337c3158
jamydewaal <35739@ma-web.nl>
2022-12-14 14:42:42 +0100

    crazy

--- web/les2/script.js
+++ web/les2/script.js
@@ -1,17 +1,22 @@
 console.log("script is geladen");
 
-const groentevak = document.getElementById('Groente');
+const groentevak = document.getElementById('groente');
 const fruitvak = document.getElementById('fruit');
 
 function zetInFruitLa(fruit){
     console.log(fruit);
 
     const nieuwElement = document.createElement('h2');
-    nieuwElement.innerHTML = 'Hoi';
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
     fruitvak.appendChild(nieuwElement);
 }
 function zetInGroenteLa(groente){
-    console.log(groente)
+    console.log(groente);
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    
+    groentevak.appendChild(nieuwElement);
 
     
 }
