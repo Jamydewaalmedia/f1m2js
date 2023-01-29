b1d239102ad33940a6bcd0a806f73fbc9a6536f9
jamydewaal <35739@ma-web.nl>
2023-01-29 11:48:24 +0100

    old work

---
+++ web/03 -tetoonstelling/script.js
@@ -0,0 +1,4 @@
+const myImage = document.getElementById('myImage')
+let paintings = ["img/plaatje1 flex.jpg","img/plaatje2 flex.jpg","img/plaatje3 flex.jpg","img/plaatje4 flex.jpg","img/plaatje5 flex.jpg"]
+function changeImage(index){
+    myImage.src = 
